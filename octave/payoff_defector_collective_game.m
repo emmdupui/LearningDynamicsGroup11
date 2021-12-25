b4ca570bee880c2b397e459f053fd2f291b33905
sirenard <renardsimon@skynet.be>
2021-12-25 10:23:55 +0100

    I forget this function

---
+++ octave/payoff_defector_collective_game.m
@@ -0,0 +1,11 @@
+function payoff=payoff_defector_collective_game(j_c)
+    global b
+    global M
+    global r
+
+    if j_c >= M
+        payoff = b;
+    else
+        payoff = (1-r)*b;
+    end
+endfunction
