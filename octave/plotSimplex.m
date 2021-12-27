8528d4e0cdbaeb10f59a0d5e8a12c8752d5c5165
emmdupui <emma.dupuis@ulb.be>
2021-12-27 14:22:00 +0100

    averageGroupAchievment

--- octave/plotSimplex.m
+++ octave/plotSimplex.m
@@ -1,4 +1,4 @@
-function plotSimplex(state_matrix)
+function plotSimplex(state_matrix, stationary_distribution)
     global Z
 
     x_src = [];
@@ -6,21 +6,32 @@ function plotSimplex(state_matrix)
     x_length = [];
     y_length = [];
     for i_e = 0 : Z
-        i_e
-        for i_c = 0 : Z - i_e
-            if mod(i_c, 3) == 0 %faster and clearer
-                line = loc_Transform(i_e, i_c);
+        if mod(i_e, 4) == 0 %faster and clearer
+            for i_c = 0 : Z - i_e
+                if mod(i_c, 4) == 0 %faster and clearer
+                    line = loc_Transform(i_e, i_c);
 
-                [x_vect, y_vect] = getSimplexVector(state_matrix, i_e, i_c);
+                    [x_vect, y_vect] = getSimplexVector(state_matrix, i_e, i_c);
 
-                x_shift = i_e/2;
+                    x_shift = i_e/2;
 
-                x_src = [x_src i_c + x_shift];
-                y_src = [y_src i_e];
-                x_length = [x_length x_vect];
-                y_length = [y_length y_vect];
+                    x_src = [x_src i_c + x_shift];
+                    y_src = [y_src i_e];
+
+                    x_shift = y_vect/2;
+                    x_length = [x_length x_vect + x_shift];
+                    y_length = [y_length y_vect];
+                end
             end
         end
     end
-    quiver(x_src, y_src, x_length, y_length);xlim([0 100]);ylim([0 100]);
+
+    quiver(x_src, y_src, x_length, y_length);xlim([-10 Z+10]);ylim([-10 Z+10]);
+    text(Z/2, Z+5, "E");
+    text(Z+5, 0 , "C");
+    text(-5, 0, "D");
+
+    average_achievement = averageGroupAchievment(stationary_distribution)*100;
+    text(Z/2-5, -5, strcat("\\eta_G = ", num2str(average_achievement), "%"));
+    axis off
 end
--- octave/probIncrease.m
+++ octave/probIncrease.m
@@ -1,6 +1,7 @@
 % c_increase -> 1 if we want to compute T^C+-, 0 if we want to compute T^E+-
 % increase -> 1 if we want to compute the increase probability
 function prob=probIncrease(state_matrix, state, i_e, i_c, c_increase, increase)
+    global Z
     if c_increase == 1
         if increase == 1
             i_c = i_c + 1;
@@ -15,13 +16,11 @@ function prob=probIncrease(state_matrix, state, i_e, i_c, c_increase, increase)
         end
     end
 
-    if i_e < 0 || i_c < 0 || i_e > 100 || i_c > 100
+    if i_e < 0 || i_c < 0 || i_e > Z || i_c > Z || i_c + i_e > Z
         prob = 0;
     else
         new_state = loc_Transform(i_e, i_c);
-        if new_state <= size(state_matrix)(1)
-            prob  = state_matrix(state, new_state);
-        end
+        prob = state_matrix(state, new_state);
     end
 
     if c_increase == 1
@@ -40,8 +39,6 @@ function prob=probIncrease(state_matrix, state, i_e, i_c, c_increase, increase)
 
     if ~(i_e < 0 || i_c < 0 || i_e > Z || i_c > Z || i_c + i_e > Z)
         new_state = loc_Transform(i_e, i_c);
-        if new_state <= size(state_matrix)(1)
-            prob = prob + state_matrix(state, new_state);
-        end
+        prob = prob + state_matrix(state, new_state);
     end
 end
